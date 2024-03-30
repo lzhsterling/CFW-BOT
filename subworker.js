5f663a71b2a6330ede007898e2322cb21e1def36
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-03-30 18:58:39 +0330

    Update subworker.js

--- subworker.js
+++ subworker.js
@@ -5,16 +5,16 @@ let mytoken= 'username';//Quick subscription access entry, leave it blank to not
 
 // Set the preferred address. The default port number is 443. Non-TLS subscription generation is not supported.
 let addresses = [
-	"go.inmobi.com#CFW-REvil",
-	"creativecommons.org#CFW-REvil",
-	"go.inmobi.com:8443#CFW",
-	"creativecommons.org:8443#CFW",
-	"go.inmobi.com:2087#CFW",
-	"www.speedtest.net:443#CFW",
-	"www.ipget.net:8443#CFW",
-	"creativecommons.org:2087#CFW",
-	"go.inmobi.com:2096#CFW",
-	"creativecommons.org:2096#CFW",
+	"go.inmobi.com#CFW-",
+	"creativecommons.org#CFW-",
+	"go.inmobi.com:8443#CFW-",
+	"creativecommons.org:8443#CFW-",
+	"go.inmobi.com:2087#CFW-",
+	"www.speedtest.net:443#CFW-",
+	"www.ipget.net:8443#CFW-",
+	"creativecommons.org:2087#CFW-",
+	"go.inmobi.com:2096#CFW-",
+	"creativecommons.org:2096#CFW-",
 	
 ];
 
